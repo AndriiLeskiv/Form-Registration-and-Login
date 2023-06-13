9a01abad2f1bab318ceeb0a9b34195b290b2deeb
Andrii <leskivandrii21@gmail.com>
2023-06-13 14:21:07 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <section class="index-login">
+        <div class="wrapper">
+            <div class="index-login-signup">
+                <h4>Sign Up</h4>
+                <p>Don't have an account yet? Sign up here!</p>
+                <form action="includes/signup.inc.php" method="post">
+                    <input type="text" name="uid" placeholder="Username">
+                    <input type="password" name="pwd" placeholder="Password">
+                    <input type="password" name="pwdRepeat" placeholder="Repeat Password">
+                    <input type="tel" name="email" placeholder="E-mail">
+                    <br>
+                    <button type="submit" name="submit">Sign Up</button>
+                </form>
+            </div>
+            <div class="index-login-login">
+                <h4>Login</h4>
+                <p>Don't have an account yet? Sign up here!</p>
+                <form action="includes/login.inc.php" method="post">
+                    <input type="text" name="uid" placeholder="Username">
+                    <input type="password" name="pwd" placeholder="Password">
+                    <br>
+                    <button type="submit" name="submit">Login</button>
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
